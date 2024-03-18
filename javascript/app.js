1f34f1ff65cb6ae2ae20e0840f06e7cc7f9e4f76
Jimi-Bada <jimibadaadedamola@gmail.com>
2024-03-18 20:13:28 +0100

    new update

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,59 @@
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  loop: true,
+  autoplay: {
+    delay: 2000, // 2 seconds
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  // breakpoints: {
+  //   // when window width is >= 992px
+  //   992: {
+  //     slidesPerView: 3
+  //   },
+  //   // when window width is >= 768px and < 992px
+  //   768: {
+  //     slidesPerView: 2
+  //   },
+  //   // when window width is < 768px
+  //   0: {
+  //     slidesPerView: 1
+  //   }
+  // }
+  // Coverflow effect
+  // effect: 'coverflow',
+  // coverflowEffect: {
+  //   rotate: 50,
+  //   stretch: 0,
+  //   depth: 100,
+  //   modifier: 1,
+  //   slideShadows: true,
+  // }
+ 
+});
+var container = document.querySelector('.swiper-container');
+      var prevButton = document.querySelector('.swiper-button-prev');
+      var nextButton = document.querySelector('.swiper-button-next');
+
+      container.addEventListener('mouseenter', function () {
+        prevButton.style.display = 'block';
+        nextButton.style.display = 'block';
+        swiper.autoplay.stop();
+      });
+
+      container.addEventListener('mouseleave', function () {
+        prevButton.style.display = 'none';
+        nextButton.style.display = 'none';
+        swiper.autoplay.start();
+      });
+
+
+      
 const navButton = document.getElementById("nav-button");
 const links = document.querySelectorAll('.nav-item a');
+
 
 
 links.forEach(function(link){
@@ -33,3 +87,5 @@ navButton.addEventListener('click', toggleBackgroundColor);
 //     link.addEventListener('click', setActiveLinks);
 // });
 // setActiveLinks();
+
+    
